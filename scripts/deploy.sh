286dc8e28aef3096adcef852c450929322efe200
조중현 <junghyun@aprd.io>
2022-06-15 15:57:51 +0900

    Fix code deploy comment and deploy shell script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,7 +29,7 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
@@ -47,4 +47,4 @@ echo "> $JAR_NAME 실행"
 nohup java -jar \
         -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
         -Dspring.profiles.active=real \
-        $$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
